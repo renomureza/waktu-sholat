3438f3aeff91a2a13f6de1556228e5481d1aad21
renomureza <renomureza@gmail.com>
2022-03-16 18:02:41 +0700

    remove header

--- src/services/prayer.service.js
+++ src/services/prayer.service.js
@@ -1,5 +1,3 @@
-const fs = require("fs/promises");
-const path = require("path");
 const { find: findTimeZone } = require("geo-tz");
 const { isValidLongitude, isValidLatitude } = require("../utils/utility");
 const ApiError = require("../utils/ApiError");
